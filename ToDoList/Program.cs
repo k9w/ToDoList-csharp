856f40e532f71cbe8e0a18971b3b5b7f14916707
Kevin Williams <kevin@k9w.org>
2022-02-23 14:22:41 -0800

    Implement initial UI in Program.cs.
    Co-authored-by: Jase Seeley <jcseeley@users.noreply.github.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,10 +1,36 @@
 using System;
-using ToDoList;
-namespace ToDoListProgram
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList
 {
   public class Program
   {
-    
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to the To Do List!\nWould you like to add an item or view your list? (Please type add or view)");
+      string response = Console.ReadLine();
+      bool add = response == "add";
+      bool view = response == "view";
+      if (add)
+      {
+        Console.WriteLine("Please enter the description of your new item.");
+        string description = Console.ReadLine();
+        List<Item> newList = new List<Item> {  };
+        newList.Add(new Item(description));
+        Console.WriteLine("'" + description + "'" + " has been added to your list.");
+        Main();
+      }
+      else if (view)
+      {
+        List<Item> result = Item.GetAll();
+        foreach (Item thisItem in result)
+        {
+          Console.WriteLine("Your list items: " + thisItem.Description);
+        }
+        Main();
+      }
+    }
   }
 }
 

34eea894783db2c62540df0726641ac711cb3187
Kevin Williams <kevin@k9w.org>
2022-02-23 15:31:48 -0800

    Finish version 1 of the interface.
    Co-authored-by: Jase Seeley <jcseeley@users.noreply.github.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,34 +1,46 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ToDoList.Models;
 
 namespace ToDoList
 {
   public class Program
   {
+    public static List<Item> newList = new List<Item> {  };
+    
     public static void Main()
     {
-      Console.WriteLine("Welcome to the To Do List!\nWould you like to add an item or view your list? (Please type add or view)");
+      Console.WriteLine("Would you like to add an item or view your list?");
       string response = Console.ReadLine();
       bool add = response == "add";
       bool view = response == "view";
+      
       if (add)
       {
         Console.WriteLine("Please enter the description of your new item.");
         string description = Console.ReadLine();
-        List<Item> newList = new List<Item> {  };
         newList.Add(new Item(description));
         Console.WriteLine("'" + description + "'" + " has been added to your list.");
         Main();
       }
       else if (view)
       {
-        List<Item> result = Item.GetAll();
-        foreach (Item thisItem in result)
+        if (newList.Count >= 1)
         {
-          Console.WriteLine("Your list items: " + thisItem.Description);
+          Console.WriteLine("Your list items: ");
+          List<Item> result = Item.GetAll();
+          foreach (Item thisItem in result)
+          {
+            Console.WriteLine(thisItem.Description);
+          }
+          Main();
         }
-        Main();
+        else
+        {
+          Console.WriteLine("There are no items on your list.");
+          Main();
+        }
       }
     }
   }

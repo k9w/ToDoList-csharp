8717d5a623ea2edf2b1852042197ac42d19fb7bb
Kevin Williams <kevin@k9w.org>
2022-02-23 09:54:11 -0800

    Test for updating description. Pass the test by adding a setter.
    Co-authored-by: Jase Seeley <jcseeley@users.noreply.github.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -23,6 +23,17 @@ namespace ToDoList.Tests
       Assert.AreEqual(description, result);
     }
 
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string updatedDescription = "Do the dishes.";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+      Assert.AreEqual(updatedDescription, result);
+    }
+
   }
 }
 
